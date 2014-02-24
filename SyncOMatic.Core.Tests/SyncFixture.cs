0a5ad16338dbc3fa7700b0143ad91faef1312a43
Andreas Öhlund <andreas.ohlund@particular.net>
2014-02-24 15:04:44 +0100

    Adding ServiceInsight to the list of repos to sync

--- SyncOMatic.Core.Tests/SyncFixture.cs
+++ SyncOMatic.Core.Tests/SyncFixture.cs
@@ -7,58 +7,10 @@ namespace SyncOMatic.Core.Tests
     [TestFixture]
     public class SyncFixture
     {
-        private SyncOMatic BuildSUT()
-        {
-            return new SyncOMatic(Helper.Credentials, Helper.Proxy, DiffFixture.ConsoleLogger);
-        }
-
-        [Test]
-        public void SyncScenario()
-        {
-            var gitIgnoreFile = new Parts("Particular/RepoStandards", TreeEntryTargetType.Blob, "master", ".gitignore");
-            var gitAttributesFile = new Parts("Particular/RepoStandards", TreeEntryTargetType.Blob, "master", ".gitattributes");
-            var dotSettingsFile = new Parts("Particular/RepoStandards", TreeEntryTargetType.Blob, "master", "src/RepoName.sln.DotSettings");
-            var buildSupportFolder = new Parts("Particular/RepoStandards", TreeEntryTargetType.Tree, "master", "buildsupport");
-
-            var map = new Mapper()
-                .Add(gitIgnoreFile,
-                    new Parts("Particular/SyncOMatic", TreeEntryTargetType.Blob, "develop", ".gitignore"),
-                    new Parts("Particular/GitFlowVersion", TreeEntryTargetType.Blob, "master", ".gitignore"),
-                    new Parts("Particular/NServiceBus", TreeEntryTargetType.Blob, "develop", ".gitignore"),
-                    new Parts("Particular/ServiceControl", TreeEntryTargetType.Blob, "develop", ".gitignore")
-                )
-                .Add(gitAttributesFile,
-                    new Parts("Particular/SyncOMatic", TreeEntryTargetType.Blob, "develop", ".gitattributes"),
-                    new Parts("Particular/GitFlowVersion", TreeEntryTargetType.Blob, "master", ".gitattributes"),
-                    new Parts("Particular/NServiceBus", TreeEntryTargetType.Blob, "develop", ".gitattributes"),
-                    new Parts("Particular/ServiceControl", TreeEntryTargetType.Blob, "develop", ".gitattributes")
-                )
-                .Add(dotSettingsFile,
-                    new Parts("Particular/GitFlowVersion", TreeEntryTargetType.Blob, "master", "GitFlowVersion.sln.DotSettings"),
-                    new Parts("Particular/NServiceBus", TreeEntryTargetType.Blob, "develop", "src/NServiceBus.sln.DotSettings"),
-                    new Parts("Particular/ServiceControl", TreeEntryTargetType.Blob, "develop", "src/ServiceControl.sln.DotSettings")
-                )
-                .Add(buildSupportFolder,
-                    new Parts("Particular/NServiceBus", TreeEntryTargetType.Tree, "develop", "buildsupport"),
-                    new Parts("Particular/ServiceControl", TreeEntryTargetType.Tree, "develop", "buildsupport")
-                )
-                ;
-
-            using (var som = BuildSUT())
-            {
-                var diff = som.Diff(map);
-                Assert.NotNull(diff);
-
-                foreach (var url in som.Sync(diff, SyncOutput.CreateCommit))
-                {
-                    //Console.WriteLine(url);
-                }
-            }
-        }
-
-
         [TestCase("ServiceMatrix","develop","src",null,null)]
-        public void PerformRepoSync(string repoName,string defaultBranch,string srcRoot, string solutionName, List<SyncItem> itemsToSync)
+        [TestCase("NServiceBus", "develop", "src", null, null)]
+        [TestCase("ServiceInsight", "develop", "src", null, null)]
+        public void PerformRepoSync(string repoName, string defaultBranch, string srcRoot, string solutionName, List<SyncItem> itemsToSync)
         {
             if (itemsToSync == null)
             {
@@ -80,12 +32,10 @@ namespace SyncOMatic.Core.Tests
 
 
 
-            using (var som = BuildSUT())
+            using (var som = new SyncOMatic(Helper.Credentials, Helper.Proxy, DiffFixture.ConsoleLogger))
             {
                 var diff = som.Diff(toSync.GetMapper(itemsToSync));
                 Assert.NotNull(diff);
-
-                
 
                 foreach (var url in som.Sync(diff, SyncOutput.CreateBranch))
                 {
