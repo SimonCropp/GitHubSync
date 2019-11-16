e567a88288b7fa280f81f55507829d1d2d5b663e
Simon Cropp <simon.cropp@gmail.com>
2019-11-16 22:15:30 +1100

    add helper

---
+++ src/Tests/CleanupTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+[Trait("Category", "Integration")]
+public class CleanupTests :
+    XunitApprovalBase
+{
+    [Fact]
+    public async Task Run()
+    {
+        var existing = await Client.GitHubClient.Repository.Branch.GetAll("SimonCropp", "GitHubSync.TestRepository");
+        foreach (var branch in existing.Where(x => x.Name.StartsWith("GitHubSync-")))
+        {
+            await Client.DeleteBranch(branch.Name);
+        }
+    }
+
+    public CleanupTests(ITestOutputHelper output) :
+        base(output)
+    {
+    }
+}
---
+++ src/Tests/Client.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Octokit;
+
+public static class Client
+{
+    public static GitHubClient GitHubClient = new GitHubClient(new ProductHeaderValue("GitHubSync"))
+    {
+        Credentials = CredentialsHelper.Credentials
+    };
+
+    public static async Task DeleteBranch(string branchName)
+    {
+        var existing = await GitHubClient.Repository.Branch.GetAll("SimonCropp", "GitHubSync.TestRepository");
+        if (existing.Any(x=>x.Name==branchName))
+        {
+            await GitHubClient.Git.Reference.Delete("SimonCropp", "GitHubSync.TestRepository", $"heads/{branchName}");
+        }
+    }
+}
