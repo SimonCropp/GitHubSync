d6b4b5ea43c1fa17b0efff3e5c5c409968964e28
SimonCropp <simon.cropp@gmail.com>
2019-11-25 21:27:16 +1100

    move to verify

---
+++ src/Tests/GlobalSetup.cs
@@ -0,0 +1,15 @@
+﻿using VerifyXunit;
+using Xunit;
+
+[GlobalSetUp]
+public static class GlobalSetup
+{
+    public static void Setup()
+    {
+        Global.ModifySerialization(settings =>
+        {
+            settings.IgnoreMembersThatThrow(x => x.Message == "Cannot escape out of a Tree.");
+            settings.IgnoreMember<Parts>(x => x.Sha);
+        });
+    }
+}
