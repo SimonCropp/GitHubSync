8b2af18230f4e44013b9638d7ebe31ce8f240f23
Geert van Horrik <GeertvanHorrik@users.noreply.github.com>
2020-02-23 07:55:10 +0100

    Allow running GitHubSync from yaml config (#61)

---
+++ src/GitHubSync/Config/ContextLoader.cs
@@ -0,0 +1,30 @@
+﻿namespace Config
+{
+    using Octokit;
+    using System;
+    using System.IO;
+    using YamlDotNet.Serialization;
+    using YamlDotNet.Serialization.NamingConventions;
+
+    internal static class ContextLoader
+    {
+        internal static Context Load(string fileName)
+        {
+            var configurationContent = File.ReadAllText(fileName);
+
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(new CamelCaseNamingConvention())
+                .Build();
+
+            var context = deserializer.Deserialize<Context>(configurationContent);
+
+            var githubToken = Environment.GetEnvironmentVariable("Octokit_OAuthToken");
+            if (!string.IsNullOrWhiteSpace(githubToken))
+            {
+                context.Credentials = new Credentials(githubToken);
+            }
+
+            return context;
+        }
+    }
+}
---
+++ src/GitHubSync/Config/Repository.cs
@@ -0,0 +1,29 @@
+﻿namespace Config
+{
+    using System.Collections.Generic;
+
+    internal class Repository
+    {
+        public Repository()
+        {
+            Branch = "master";
+
+            Templates = new List<string>();
+        }
+
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public string Branch { get; set; }
+
+        public bool AutoMerge { get; set; }
+
+        public List<string> Templates { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Url})";
+        }
+    }
+}
