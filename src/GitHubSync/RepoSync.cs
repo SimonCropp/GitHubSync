6f04dae7a2576f80df94542600d0af3eba27c9e4
Simon Cropp <simon.cropp@gmail.com>
2022-03-11 11:07:24 +1100

    dont add null result to sync output

--- src/GitHubSync/RepoSync.cs
+++ src/GitHubSync/RepoSync.cs
@@ -252,8 +252,8 @@ public class RepoSync
             else
             {
                 log($"Pull created for {targetRepositoryDisplayName}, click here to review and pull: {createdSyncBranch}");
+                list.Add(createdSyncBranch);
             }
-            list.Add(createdSyncBranch);
         }
 
         return list;
